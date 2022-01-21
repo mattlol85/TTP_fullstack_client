691f921d857430f16c2936a9410615f748fb8e37
Matthew Fitzgerald <mattlol85@gmail.com>
2022-01-21 13:08:47 -0500

    Added header and footer in component folder' '

---
+++ kampus/src/components/Footer.js
@@ -0,0 +1,19 @@
+//import React from 'react';
+import './styles/Footer.css';
+
+export default function Footer() {
+
+
+    return (
+        <div id='footer' >
+            <div id='footerItems'>
+                Jason Charles   |
+                Matthew Fitzgerald  |
+                Zijinhui Zhuang
+            </div>
+            <div id='bottomOfFooter' className='center' className='centerText'>
+                Ⓒ Group 10
+            </div>
+        </div>
+    );
+}
