f7017f85aeddc55cc4c08ae43b32b4877a466605
Matthew Fitzgerald <mattlol85@gmail.com>
2022-01-24 00:37:18 -0500

    Added a few more needed components, campus page broke

--- kampus/src/components/StudentCard.js
+++ kampus/src/components/StudentCard.js
@@ -15,14 +15,13 @@ export default function StudentCard({props}) {
         .catch(err =>{
             console.log(err);
         })
-    }, [])
-
+    }, []);
+    
     return (
         <div className="studentCard">
             <img src="https://www.fakepersongenerator.com/Face/female/female102160866071.jpg" alt="John"/>
                 <h1>Firstname Lastname</h1>
                 <p>Harvard University</p>
-                <p></p>
         </div>
 
     );
